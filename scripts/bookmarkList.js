725f45436b4e08e867685aea358b4b0c0f73c771
Dylan <nielsen.dylan@gmail.com>
2018-01-25 08:41:00 -0800

    Add rating to HTML generator

--- scripts/bookmarkList.js
+++ scripts/bookmarkList.js
@@ -3,27 +3,33 @@
 /* global api, index, store */
 
 const bookmarkList = (function() {
-
   const render = function() {
     const renderedBookmarks = renderBookmarks(store.bookmarks);
     $('.bookmark-content').html(renderedBookmarks);
   };
 
   const renderBookmarks = function(bookmarks) {
-    return bookmarks.map((bookmark) => {
+    return bookmarks.map(bookmark => {
       return generateBookmarkHTML(bookmark);
     });
   };
 
   const generateBookmarkHTML = function(bookmark) {
+    let rating = `
+      <span class="fa fa-star"></span>
+      <span class="fa fa-star"></span>
+      <span class="fa fa-star"></span>
+      <span class="fa fa-star"></span>
+      <span class="fa fa-star"></span>`;
+    if (bookmark.rating !== null) {
+      rating =
+        '<span class="fa fa-star checked" />\n'.repeat(bookmark.rating) +
+        '<span class="fa fa-star" />\n'.repeat(5 - bookmark.rating);
+    }
     return `
     <div class='bookmark item-id'>
       <h2>${bookmark.title}</h2>
-      <span class="fa fa-star"></span>
-      <span class="fa fa-star"></span>
-      <span class="fa fa-star"></span>
-      <span class="fa fa-star"></span>
-      <span class="fa fa-star"></span>
+      ${rating}
     </div>
     `;
   };
